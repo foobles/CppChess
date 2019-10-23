36b5387172362689bd71963bb136e47ec2eb06c7
foobles <buzdav2@gmail.com>
2019-10-23 13:43:17 -0500

    Updated move parsing (+debug get command)

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,11 @@ Point string_to_point(std::string const& str) {
     };
 }
 
-std::unique_ptr<chess::Action> get_input(Team cur_team) {
+void print_point(Point p) {
+    std::cout << '{' << p.x << ", " << p.y << '}';
+}
+
+std::unique_ptr<chess::Action> get_input(Team cur_team, Chess* ch = nullptr) {
     std::string str;
     while (str.empty()) {
         std::getline(std::cin, str);
@@ -31,6 +35,7 @@ std::unique_ptr<chess::Action> get_input(Team cur_team) {
     std::smatch sm;
     std::regex rg_move(R"(^\s*([A-Ha-h][1-8])\s+to\s+([A-Ha-h][1-8])\s*$)");
     std::regex rg_pass(R"(^\s*pass\s*$)");
+    std::regex rg_get_moves(R"(^get\s+([A-Ha-h][1-8])\s*$)");
     if (std::regex_search(str, sm, rg_move)) {
         return std::make_unique<chess::action::Move>(
                 cur_team,
@@ -38,18 +43,19 @@ std::unique_ptr<chess::Action> get_input(Team cur_team) {
                 string_to_point(sm[2].str()));
     } else if (std::regex_match(str, rg_pass)) {
         return std::make_unique<chess::action::Pass>(cur_team);
-    } else {
-        throw ParseInputError();
     }
+    throw ParseInputError();
+
 }
 
 int main() {
     Chess chess_game;
 
+
     while (true) {
         chess_game.board().draw(chess_game.cur_team());
         try {
-            get_input(chess_game.cur_team())->handle(chess_game);
+            get_input(chess_game.cur_team(), &chess_game)->handle(chess_game);
             if (([]{return false;})()) break;
         } catch (ParseInputError const& e) {
             std::cout << "Bad input.\n";
@@ -59,7 +65,7 @@ int main() {
                 std::cout << "You aren't allowed to move that there.\n";
                 break;
             case chess::RuleException::Type::NoPiece:
-                std::cout << "There is no piece there.\n";
+                std::cout << "You have no piece there.\n";
                 break;
             case chess::RuleException::Type::WrongTeam:
                 std::cout << "You can only move your own pieces.\n";
